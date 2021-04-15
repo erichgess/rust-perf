8a9911ae02358e34ffea863b0680c73d6b837305
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-04-14 20:01:33 -0400

    Sum where I read from a different cacheline in each iteration

--- heap-view/src/main.rs
+++ heap-view/src/main.rs
@@ -2,19 +2,37 @@ use std::time::Instant;
 
 fn main() {
     linear_sum();
-
     bounce();
-
+    skipper_sum();
     linear_sum();
 }
 
-fn linear_sum() -> f64 {
-    let a = Box::new([1.; 100_000]);
+fn skipper_sum() -> f64 {
+    let n = 100_000;
+    let a = vec![1.; n];
     let mut sum = 0.;
 
     let time = Instant::now();
-    for i in 0..100_000 {
+    let n = n / 10;
+    for i in 0..10 {
+        for j in 0..n {
+            sum += a[j * 10 + i];
+        }
+    }
+    let d = time.elapsed().as_nanos();
+    println!("{} in {}ns", sum, d); /* */
+
+    sum
+}
+
+fn linear_sum() -> f64 {
+    let a = vec![1.; 100_000];
+    let mut sum = 0.;
+
+    let time = Instant::now();
+    for i in 0..50_000 {
         sum += a[i];
+        sum += a[i + 1];
     }
     let d = time.elapsed().as_nanos();
     println!("{} in {}ns", sum, d); /* */
@@ -23,14 +41,14 @@ fn linear_sum() -> f64 {
 }
 
 fn bounce() -> f64 {
-    let a = Box::new([1.; 100_000]);
+    let a = vec![1.; 50_000];
+    let b = vec![1.; 50_000];
     let mut sum = 0.;
 
     let time = Instant::now();
     for i in 0..100_000 / 2 {
         sum += a[i];
-        let j = 100_000 - i - 1;
-        sum += a[j];
+        sum += b[i];
     }
     let d = time.elapsed().as_nanos();
     println!("{} in {}ns", sum, d); /* */
