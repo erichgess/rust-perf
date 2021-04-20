72e98a905d300bcf6ecfb5ee79db4d083603e91c
Erich Ess <erich.g.ess@gmail.com>
2021-04-20 13:08:09 -0400

    Simplify code down to somethign I can post on StackOverflow

--- basic_threads/src/main.rs
+++ basic_threads/src/main.rs
@@ -1,109 +1,40 @@
 use std::thread;
 use std::time::Instant;
 
-#[derive(Debug)]
-struct Test {
-    id: usize,
-    time_ms: u128,
-    thread_ms: Vec<(usize, f64, u128)>,
-}
-
-#[derive(Debug)]
-struct TestSet {
-    threads: usize,
-    loops: usize,
-    time: u128,
-    tests: Vec<Test>,
-}
-
 fn main() {
-    print_test_set_header();
-    let tests = [5_000_000, 50_000_000, 500_000_000];
+    let loops = 100_000_000;
 
-    for loops in &tests {
-        for threads in 1..=8 {
-            let ts = n_threads(threads, *loops, 2);
-            print_test_set(*loops, "max", &ts);
-        }
-    }
-
-    for loops in &tests {
-        for threads in 1..=8 {
-            let ts = n_threads(threads, *loops / threads, 2);
-            print_test_set(*loops, "split", &ts);
-        }
+    for threads in 1..=8 {
+        n_threads(threads, loops / threads);
     }
 }
 
-fn print_test_set_header() {
-    println!("Test,Type,Th,Loops,Set Sz,Set Time,Avg,Min,Max");
-}
-
-fn print_test_set(test: usize, ty: &str, tests: &TestSet) {
-    let num_tests = tests.tests.len();
-    let thread_times: Vec<u128> = tests
-        .tests
-        .iter()
-        .flat_map(|t| t.thread_ms.iter())
-        .map(|(_, _, dt)| *dt)
-        .collect();
-    let avg = thread_times.iter().sum::<u128>() as f64 / thread_times.len() as f64;
-    let max = thread_times.iter().max().unwrap();
-    let min = thread_times.iter().min().unwrap();
-
-    println!(
-        "{},{},{},{},{},{},{:.2},{},{}",
-        test, ty, tests.threads, tests.loops, num_tests, tests.time, avg, min, max
-    );
-}
-
-fn n_threads(num_threads: usize, loops: usize, tests: usize) -> TestSet {
-    let mut thread_stats = vec![];
-    let set_timer = Instant::now();
-    for _ in 0..tests {
-        let sw = Instant::now();
-        let mut threads = Vec::new();
-        for idx in 0..num_threads {
-            let t = thread::spawn(move || {
-                let (v, t) = work(loops);
-                (idx, v, t)
-            });
-            threads.push(t);
-        }
-
-        let mut results = Vec::new();
-        for t in threads.into_iter() {
-            match t.join() {
-                Ok((idx, v, t)) => {
-                    results.push((idx, v, t));
-                }
-                Err(e) => panic!("{:?}", e),
-            }
-        }
-        let time = sw.elapsed();
-        let ts = results.iter().map(|(idx, v, t)| (*idx, *v, *t)).collect();
-        thread_stats.push(Test {
-            id: 0,
-            time_ms: time.as_millis(),
-            thread_ms: ts,
-        });
-    }
-
-    TestSet {
-        loops,
-        threads: num_threads,
-        tests: thread_stats,
-        time: set_timer.elapsed().as_millis(),
-    }
-}
-
-fn work(loops: usize) -> (f64, u128) {
-    let mut x = 0.5;
+fn n_threads(num_threads: usize, loops: usize) {
     let sw = Instant::now();
+
+    let mut threads = Vec::new();
+    for idx in 0..num_threads {
+        let t = thread::spawn(move || {
+            let v = work(loops);
+            (idx, v)
+        });
+        threads.push(t);
+    }
+
+    for t in threads.into_iter() {
+        t.join().unwrap();
+    }
+
+    let time = sw.elapsed();
+    println!("{}, {}", num_threads, time.as_millis());
+}
+
+fn work(loops: usize) -> f64 {
+    let mut x = 0.5;
+
     for i in 0..loops {
         x += (i as f64 / 10000.).sin();
     }
-    let dur = sw.elapsed().as_millis();
 
-    (x, dur)
+    x
 }
